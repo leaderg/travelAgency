486b20fa28f7fc193327c89878c3428c01556700
Greg <leader.g@gmail.com>
2020-12-11 13:45:50 -0700

    changing ports

--- server.js
+++ server.js
@@ -3,22 +3,13 @@ const path = require('path');
 
 const app = express();
 
-// Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'travel-agency/build')));
 
-// An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
-});
-
-// Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/travel-agency/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
 
-console.log('App is listening on port ' + port);
+console.log('App is listening on port ' + PORT);
